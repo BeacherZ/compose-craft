46f8603f1f2de9d9d519392ab508f0612fd3639c
BeacherZ <60752063+BeacherZ@users.noreply.github.com>
2026-08-12 22:01:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 VOLUME_PREFIX="${VOLUME_PREFIX:-/opt/docker/{container_name}}"
-sed -i "s|/opt/docker/{container_name}|${VOLUME_PREFIX}|g" /usr/share/nginx/html/index.html
+sed -i "s|/opt/docker/\{container_name\}|${VOLUME_PREFIX}|g" /usr/share/nginx/html/index.html
 nginx -g 'daemon off;'
