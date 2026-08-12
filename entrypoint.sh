a38148eba47bf3f936e443e7a5b79313a6f1b35f
BeacherZ <60752063+BeacherZ@users.noreply.github.com>
2026-08-12 22:32:46 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-VOLUME_PREFIX="${VOLUME_PREFIX:-/opt/docker/{container_name}}"
-cat > /usr/share/nginx/html/config.js << EOF
-window.VOLUME_PREFIX = '${VOLUME_PREFIX}';
-EOF
+if [ -z "$VOLUME_PREFIX" ]; then
+    VOLUME_PREFIX='/opt/docker/{container_name}'
+fi
+printf 'window.VOLUME_PREFIX = "%s";\n' "$VOLUME_PREFIX" > /usr/share/nginx/html/config.js
 nginx -g 'daemon off;'
